1283166308606438bcdcdda14fac9b515e9e6102
jhchen6 <jhchen6@users.noreply.github.com>
2018-04-28 11:45:39 +0800

    minor fix

--- main.js
+++ main.js
@@ -1,45 +1,40 @@
 //the RegExp is from https://www.jasondavies.com/wordcloud/cloud.min.js
 var commonWords = /^(i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall)$/;
 
-var text,
-    max,
+var max,
     spiral, //
     scale,
     font,
-    angleCount,
-    angleFrom,
-    angleTo,
     words,
-    svg,
-    mask,
     g,
-    constant,
     svgNS = "http://www.w3.org/2000/svg",
-    canvas,
+    ctx,
+    tmpCtx,
+    canvasW,
+    canvasH,
     halfCanvasW,
     halfCanvasH,
-    ctx,
-    radian = Math.PI / 180;
+    calSize,
+    calColor,
+    calRotate;
 
-
-var tmpCanvas,
-    tmpCtx;
 $(document).ready(function () {
-    svg = $("svg")[0];
-    mask = $("mask")[0];
-    g = $("g")[0];
+    g = document.createElementNS(svgNS, "g");
+    $(g).attr("transform", 'translate(480, 300)');
+    $("svg")[0].appendChild(g);
 
-    canvas = document.createElement("canvas");
+    var canvas = document.createElement("canvas");
     $(canvas).attr("width", 960)
         .attr("height", 600);
     ctx = canvas.getContext("2d");
     ctx.translate(480, 300);
     ctx.textAlign = "center";
+    canvasW = canvas.width;
+    canvasH = canvas.height;
+    halfCanvasW = canvasW / 2;
+    halfCanvasH = canvasH / 2;
 
-    halfCanvasW = canvas.width / 2;
-    halfCanvasH = canvas.height / 2;
-
-    tmpCanvas = document.createElement("canvas");
+    var tmpCanvas = document.createElement("canvas");
     $(tmpCanvas).attr("width", 960)
         .attr("height", 600);
     tmpCtx = tmpCanvas.getContext("2d");
@@ -54,31 +49,36 @@ $(document).ready(function () {
         go();
         return false;
     });
+
     init();
     go();
 });
 
 function init() {
-    text = $("#text").val();
+    var text = $("#text").val(),
+        angleCount = +$("#angle-count").val(),
+        angleFrom = +$("#angle-from").val(),
+        angleTo = +$("#angle-to").val();
+    if (angleFrom < -90) angleFrom = -90;
+    if (angleTo > 90) angleTo = 90;
+
     max = +$("#max").val();
     spiral = spirals[$("input[name='spiral']").val()];
     scale = scales[$("input[name='scale']").val()];
     font = $("#font").val();
-    angleCount = +$("#angle-count").val();
-    angleFrom = +$("#angle-from").val();
-    if (angleFrom < -90) angleFrom = -90;
-    angleTo = +$("#angle-to").val();
-    if (angleTo > 90) angleTo = 90;
     words = [];
     g.innerHTML = "";
-    ctx.clearRect(-halfCanvasH, -halfCanvasH, canvas.width, canvas.height);
+    ctx.clearRect(-halfCanvasH, -halfCanvasH, canvasW, canvasH);
     ctx.fillStyle = ctx.strokeStyle = "red";
     tmpCtx.fillStyle = tmpCtx.strokeStyle = "red";
+
+    prepareWords(text);
+    calSize = prepareCalSize();
+    calColor = prepareCalColor();
+    calRotate = getCalRotate(angleCount, angleFrom, angleTo);
 }
 
 function go() {
-
-    prepareWords();
     var numPlaced = 0,
         len = words.length,
         word;
@@ -94,18 +94,19 @@ function go() {
     display();
 }
 
-function prepareWords() {
+function prepareWords(text) {
     //split text into words
     var rawWords = text.match(/\b(\w+)\b/g),
         wordCounts = {},
         len = rawWords.length,
         word;
+
     for (var i = 0; i < len; i++) {
         word = rawWords[i];
         word = word.toLowerCase();
 
         //leave out too common words like "the"
-        if (commonWords.test(word)) return true;
+        if (commonWords.test(word)) continue;
 
         if (wordCounts[word] === undefined) {
             wordCounts[word] = 1;
@@ -121,21 +122,20 @@ function prepareWords() {
     words.sort(function (a, b) {
         return b.count - a.count;
     });
-
-    // console.log(words);
-    constant = (100 - 10) / (scale(words[0].count) - scale(words[words.length - 1].count));
 }
 
 function place(word) {
-    var angleSlice = (angleTo - angleFrom) / (angleCount - 1),
-        collision,
-        thetaIncrement = Math.PI / 4,
+    var collision,
+        thetaIncrement = Math.PI / 8,
         theta = thetaIncrement,
         origin = {},
         r2,
         r2Max;
 
     word.size = calSize(word);
+    word.rotate = calRotate(word);
+
+    //init word position
     word.x = (Math.random() * 2 - 1) * 64; // ??
     word.y = (Math.random() * 2 - 1) * 64;
     origin.x = word.x;
@@ -147,11 +147,8 @@ function place(word) {
         calR2({ x: -halfCanvasW, y: halfCanvasH }, origin),
         calR2({ x: halfCanvasW, y: halfCanvasH }, origin)
     );
-    word.rotate = Math.floor(Math.random() * angleCount) * angleSlice + angleFrom;
-
     collision = detectCollision(word);
 
-    //
     while (!(!collision.overlap && collision.inScope) && r2 <= r2Max) {
         //calculate the position
         word.x = spiral(theta) * Math.cos(theta) + origin.x;
@@ -168,14 +165,12 @@ function place(word) {
         return;
     }
 
-    //generate fill color
-    word.fill = "rgb(" + Math.random() * 256 + ", " +
-        Math.random() * 256 + "," + Math.random() * 256 + ")";
+    word.fill = calColor(word);
 
     ctx.save();
     ctx.font = word.size + "px " + font;
     ctx.translate(word.x, word.y);
-    ctx.rotate(word.rotate * radian);
+    ctx.rotate(word.rotate);
     ctx.fillText(word.text, 0, 0);
     ctx.restore();
 
@@ -202,29 +197,30 @@ function detectCollision(word) {
         dy = word.bounding.y,
         w = word.bounding.w,
         h = word.bounding.h,
-        x = dx + word.x + halfCanvasW,
-        y = dy + word.y + halfCanvasH;
+        rectX = word.x + dx,
+        rectY = word.y + dy,
+        absX = rectX + halfCanvasW,
+        absY = rectY + halfCanvasH;
 
-    if (word.x + dx < -halfCanvasW || word.x + dx + w > halfCanvasW ||
-        word.y + dy < -halfCanvasH || word.y + dy + h > halfCanvasH) {
-        word.inScope = false;
-        word.overlap = undefined;
+    if (absX < 0 || absX + w > canvasW ||
+        absY < 0 || absY + h > canvasH) {
+        // word.inScope = false;
+        // word.overlap = undefined;
         return { inScope: false, overlap: undefined };
     }
 
     ctx.save();
     ctx.translate(word.x, word.y);
-    var pixels = ctx.getImageData(x, y, w, h).data; //this use absolute coords = =...
+    var pixels = ctx.getImageData(absX, absY, w, h).data; //this uses absolute coords = =...
     ctx.restore();
 
-
     tmpCtx.save();
-    tmpCtx.clearRect(-halfCanvasW, -halfCanvasH, canvas.width, canvas.height);
+    tmpCtx.clearRect(-halfCanvasW, -halfCanvasH, canvasW, canvasH);
     tmpCtx.font = word.size + "px " + font;
     tmpCtx.translate(word.x, word.y);
-    tmpCtx.rotate(word.rotate * radian);
+    tmpCtx.rotate(word.rotate);
     tmpCtx.fillText(word.text, 0, 0);
-    var tmpPixels = tmpCtx.getImageData(x, y, w, h).data;
+    var tmpPixels = tmpCtx.getImageData(absX, absY, w, h).data;
     tmpCtx.restore();
 
     // var countTmp = 0,
@@ -232,8 +228,8 @@ function detectCollision(word) {
     var len = pixels.length;
     for (var i = 0; i < len; i += 4) {
         if (tmpPixels[i] && pixels[i]) {
-            word.inScope = true;
-            word.overlap = true;
+            // word.inScope = true;
+            // word.overlap = true;
             return { inScope: true, overlap: true };
         }
         //     if (tmpPixels[i]) {
@@ -246,8 +242,9 @@ function detectCollision(word) {
 
     // console.log(pixels.length, count);
     // console.log(tmpPixels.length, countTmp);
-    word.inScope = true;
-    word.overlap = false;
+
+    // word.inScope = true;
+    // word.overlap = false;
 
     return { inScope: true, overlap: false };
 }
@@ -257,8 +254,8 @@ function calBoundingRect(word) {
     ctx.font = word.size + "px " + font;
     var textW = ctx.measureText(word.text).width,
         textH = word.size,
-        sin = Math.abs(Math.sin(word.rotate * radian)),
-        cos = Math.abs(Math.cos(word.rotate * radian)),
+        sin = Math.abs(Math.sin(word.rotate)),
+        cos = Math.abs(Math.cos(word.rotate)),
         boundingW = textW * cos + textH * sin,
         boundingH = textW * sin + textH * cos,
         boundingY = - boundingH + textW / 2 * sin,
@@ -272,20 +269,32 @@ function calBoundingRect(word) {
         x: boundingX
     }
     ctx.restore();
+
+    // tmpCtx.save();
+    // tmpCtx.clearRect(-halfCanvasW, -halfCanvasH, canvasW, canvasH);
+    // tmpCtx.font = word.size + "px " + font;
+    // tmpCtx.rotate(word.rotate);
+    // tmpCtx.fillText(word.text, 0, 0);
+    // var tmpPixels = tmpCtx.getImageData(boundingX + halfCanvasW, 
+    //     boundingY + halfCanvasH, boundingW, boundingH).data;
+    // tmpCtx.restore();
+
+    // for(var i=0;i<boundingH)
 }
 
 function display() {
     var text,
         numPlaced = 0,
         len = words.length,
-        word;
+        word,
+        radian = Math.PI / 180;
     for (var i = 0; i < len; i++) {
         word = words[i];
         if (word.placed == false) continue;
         text = document.createElementNS(svgNS, "text");
         $(text)
             .attr("transform", 'translate(' + [word.x, word.y] +
-                ') rotate(' + word.rotate + ')')
+                ') rotate(' + (word.rotate / radian) + ')')
             .attr("text-anchor", "middle")
             .css({
                 "font-size": word.size + "px",
@@ -332,10 +341,10 @@ function display() {
 
 var spirals = {
     archimedean: function (theta) {
-        return theta * 1;
+        return theta * 5;
     },
-    rectangular: function (theta) {
-        return theta * 1;
+    rectangular: function (theta) { //TODO
+        return theta * 5;
     }
 };
 
@@ -351,6 +360,44 @@ var scales = {
     }
 };
 
-function calSize(word) {
-    return Math.round(constant * (scale(word.count) - scale(words[0].count))) + 100;
+function prepareCalSize() {
+    var domain = {
+        min: scale(words[words.length - 1].count),
+        max: scale(words[0].count)
+    },
+        range = { min: 10, max: 100 },
+        constant = (range.min - range.max) / (domain.min - domain.max);
+
+    return function (word) {
+        return constant * (scale(word.count) - domain.max) + range.max;
+    }
+
+    // calColor = function (word) { //grey scale...
+    //     return "rgb(" + colorScale(word) + ", " +
+    //         colorScale(word) + "," + colorScale(word) + ")";
+    // }
+
+    // calColor = function (word) { //avoid 255,255,255
+    //     return "rgb(" + Math.random() * 255 + ", " +
+    //         Math.random() * 255 + "," + Math.random() * 255 + ")";
+    // }
+
+    // function colorScale(word) {
+    //     return colorConst * (scale(word.count) - domain.max) + colorRange.max;
+    // }
+}
+
+function prepareCalColor() {
+    return function (word) { //avoid 255,255,255
+        return "rgb(" + Math.random() * 255 + ", " +
+            Math.random() * 255 + "," + Math.random() * 255 + ")";
+    }
+}
+
+function getCalRotate(angleCount, angleFrom, angleTo) {
+    var angleSlice = (angleTo - angleFrom) / (angleCount - 1),
+        radian = Math.PI / 180;
+    return function (word) {
+        return (Math.floor(Math.random() * angleCount) * angleSlice + angleFrom) * radian;
+    }
 }
